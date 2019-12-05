fef3d21eadd0f5564744610aeeddb189e6817033
Irvingfu <1595118662@qq.com>
2019-12-05 12:21:42 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// 轮播图
+window.onload = function() {
+ 
+
+    var mySwiper = new Swiper ('.swiper-container', {
+     
+        loop: true, // 循环模式选项
+        autoplay:true,
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+       
+      })     
+         
+    } 
